ea89296355b28250d5dafb19a1464333e1f4863e
Hind Montassif <hind.montassif@gmail.com>
2025-06-24 16:24:25 +0200

    Add header

---
+++ include/sparrow-ipc.hpp
@@ -0,0 +1,3 @@
+#include "../include/config/config.hpp"
+
+SPARROW_IPC_API void fake_func_for_now();
--- src/sparrow-ipc.cpp
+++ src/sparrow-ipc.cpp
@@ -1,5 +1,6 @@
 #include "sparrow/sparrow.hpp"
 
+#include "../include/sparrow-ipc.hpp"
 #include "../generated/Schema_generated.h"
 
 void fake_func_for_now()
