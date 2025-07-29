1c4eeba86547819b7ba4232039cee6ef2b133f75
Hind Montassif <hind.montassif@gmail.com>
2025-07-29 10:31:38 +0200

    Add namespaces

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,19 +5,22 @@
 #include <string_view>
 #include <utility>
 
-// TODO what to do with namespace?
-// TODO add tests for these?
-// TODO add namespace ? sparrow-ipc / detail or utils?
+#include "Schema_generated.h"
+
 #include "config/config.hpp"
 
-// Aligns a value to the next multiple of 8, as required by the Arrow IPC format for message bodies.
-SPARROW_IPC_API int64_t align_to_8(int64_t n);
+// TODO add tests
+// TODO in that case define #ifdef TESTING for the fcts here to be testable and available in anonymous namespaces
+namespace sparrow_ipc
+{
+    namespace utils
+    {
+        // Aligns a value to the next multiple of 8, as required by the Arrow IPC format for message bodies
+        SPARROW_IPC_API int64_t align_to_8(int64_t n);
 
-// Parse the format string
-SPARROW_IPC_API std::optional<int32_t> parse_format(std::string_view format_str, std::string_view sep);
-
-SPARROW_IPC_API std::pair<org::apache::arrow::flatbuf::Type, flatbuffers::Offset<void>>
-get_flatbuffer_decimal_type(flatbuffers::FlatBufferBuilder& builder, std::string_view format_str, int32_t bitWidth);
-
-SPARROW_IPC_API std::pair<org::apache::arrow::flatbuf::Type, flatbuffers::Offset<void>>
-get_flatbuffer_type(flatbuffers::FlatBufferBuilder& builder, std::string_view format_str);
+        // Creates a Flatbuffers type from a format string
+        // This function maps a sparrow data type to the corresponding Flatbuffers type
+        SPARROW_IPC_API std::pair<org::apache::arrow::flatbuf::Type, flatbuffers::Offset<void>>
+        get_flatbuffer_type(flatbuffers::FlatBufferBuilder& builder, std::string_view format_str);
+    }
+}
--- tests/test.cpp
+++ tests/test.cpp
@@ -10,149 +10,148 @@
 
 #include "../include/serialize.hpp"
 
-using testing_types = std::tuple<
-    int,
-    float,
-    double>;
-
-template <typename T>
-void compare_bitmap(sparrow::primitive_array<T>& pa1, sparrow::primitive_array<T>& pa2)
-{
-    const auto pa1_bitmap = pa1.bitmap();
-    const auto pa2_bitmap = pa2.bitmap();
-
-    CHECK_EQ(pa1_bitmap.size(), pa2_bitmap.size());
-    auto pa1_it = pa1_bitmap.begin();
-    auto pa2_it = pa2_bitmap.begin();
-    for (size_t i = 0; i < pa1_bitmap.size(); ++i)
-    {
-        CHECK_EQ(*pa1_it, *pa2_it);
-        ++pa1_it;
-        ++pa2_it;
-    }
-}
-
-template <typename T>
-void compare_metadata(sparrow::primitive_array<T>& pa1, sparrow::primitive_array<T>& pa2)
-{
-    if (!pa1.metadata().has_value())
-    {
-        CHECK(!pa2.metadata().has_value());
-        return;
-    }
-
-    CHECK(pa2.metadata().has_value());
-    sparrow::key_value_view kvs1_view = *(pa1.metadata());
-    sparrow::key_value_view kvs2_view = *(pa2.metadata());
-
-    CHECK_EQ(kvs1_view.size(), kvs2_view.size());
-    std::vector<std::pair<std::string, std::string>> kvs1, kvs2;
-    auto kvs1_it = kvs1_view.cbegin();
-    auto kvs2_it = kvs2_view.cbegin();
-    for (auto i = 0; i < kvs1_view.size(); ++i)
-    {
-        CHECK_EQ(*kvs1_it, *kvs2_it);
-        ++kvs1_it;
-        ++kvs2_it;
-    }
-}
-
-TEST_CASE_TEMPLATE_DEFINE("Serialize and Deserialize primitive_array", T, primitive_array_types)
+namespace sparrow_ipc
 {
     namespace sp = sparrow;
 
-    auto create_primitive_array = []() -> sp::primitive_array<T> {
-        if constexpr (std::is_same_v<T, int>)
+    using testing_types = std::tuple<
+        int,
+        float,
+        double>;
+
+    template <typename T>
+    void compare_bitmap(sparrow::primitive_array<T>& pa1, sparrow::primitive_array<T>& pa2)
+    {
+        const auto pa1_bitmap = pa1.bitmap();
+        const auto pa2_bitmap = pa2.bitmap();
+
+        CHECK_EQ(pa1_bitmap.size(), pa2_bitmap.size());
+        auto pa1_it = pa1_bitmap.begin();
+        auto pa2_it = pa2_bitmap.begin();
+        for (size_t i = 0; i < pa1_bitmap.size(); ++i)
         {
-            return {10, 20, 30, 40, 50};
+            CHECK_EQ(*pa1_it, *pa2_it);
+            ++pa1_it;
+            ++pa2_it;
         }
-        else if constexpr (std::is_same_v<T, float>)
+    }
+
+    template <typename T>
+    void compare_metadata(sparrow::primitive_array<T>& pa1, sparrow::primitive_array<T>& pa2)
+    {
+        if (!pa1.metadata().has_value())
         {
-            return {10.5f, 20.5f, 30.5f, 40.5f, 50.5f};
+            CHECK(!pa2.metadata().has_value());
+            return;
         }
-        else if constexpr (std::is_same_v<T, double>)
+
+        CHECK(pa2.metadata().has_value());
+        sparrow::key_value_view kvs1_view = *(pa1.metadata());
+        sparrow::key_value_view kvs2_view = *(pa2.metadata());
+
+        CHECK_EQ(kvs1_view.size(), kvs2_view.size());
+        std::vector<std::pair<std::string, std::string>> kvs1, kvs2;
+        auto kvs1_it = kvs1_view.cbegin();
+        auto kvs2_it = kvs2_view.cbegin();
+        for (auto i = 0; i < kvs1_view.size(); ++i)
         {
-            return {10.1, 20.2, 30.3, 40.4, 50.5};
+            CHECK_EQ(*kvs1_it, *kvs2_it);
+            ++kvs1_it;
+            ++kvs2_it;
         }
-        else
-        {
-            FAIL("Unsupported type for templated test case");
-        }
-    };
+    }
 
-    sp::primitive_array<T> ar = create_primitive_array();
+    TEST_CASE_TEMPLATE_DEFINE("Serialize and Deserialize primitive_array", T, primitive_array_types)
+    {
+        auto create_primitive_array = []() -> sp::primitive_array<T> {
+            if constexpr (std::is_same_v<T, int>)
+            {
+                return {10, 20, 30, 40, 50};
+            }
+            else if constexpr (std::is_same_v<T, float>)
+            {
+                return {10.5f, 20.5f, 30.5f, 40.5f, 50.5f};
+            }
+            else if constexpr (std::is_same_v<T, double>)
+            {
+                return {10.1, 20.2, 30.3, 40.4, 50.5};
+            }
+            else
+            {
+                FAIL("Unsupported type for templated test case");
+            }
+        };
 
-    std::vector<uint8_t> serialized_data = serialize_primitive_array(ar);
+        sp::primitive_array<T> ar = create_primitive_array();
 
-    CHECK(serialized_data.size() > 0);
+        std::vector<uint8_t> serialized_data = serialize_primitive_array(ar);
 
-    sp::primitive_array<T> deserialized_ar = deserialize_primitive_array<T>(serialized_data);
+        CHECK(serialized_data.size() > 0);
 
-    CHECK_EQ(ar, deserialized_ar);
+        sp::primitive_array<T> deserialized_ar = deserialize_primitive_array<T>(serialized_data);
 
-    compare_bitmap<T>(ar, deserialized_ar);
-    compare_metadata<T>(ar, deserialized_ar);
-}
+        CHECK_EQ(ar, deserialized_ar);
 
-TEST_CASE_TEMPLATE_APPLY(primitive_array_types, testing_types);
+        compare_bitmap<T>(ar, deserialized_ar);
+        compare_metadata<T>(ar, deserialized_ar);
+    }
 
-TEST_CASE("Serialize and Deserialize primitive_array - int with nulls")
-{
-    namespace sp = sparrow;
+    TEST_CASE_TEMPLATE_APPLY(primitive_array_types, testing_types);
 
-    // Data buffer
-    sp::u8_buffer<int> data_buffer = {100, 200, 300, 400, 500};
+    TEST_CASE("Serialize and Deserialize primitive_array - int with nulls")
+    {
+        // Data buffer
+        sp::u8_buffer<int> data_buffer = {100, 200, 300, 400, 500};
 
-    // Validity bitmap: 100 (valid), 200 (valid), 300 (null), 400 (valid), 500 (null)
-    sp::validity_bitmap validity(5, true); // All valid initially
-    validity.set(2, false); // Set index 2 to null
-    validity.set(4, false); // Set index 4 to null
+        // Validity bitmap: 100 (valid), 200 (valid), 300 (null), 400 (valid), 500 (null)
+        sp::validity_bitmap validity(5, true); // All valid initially
+        validity.set(2, false); // Set index 2 to null
+        validity.set(4, false); // Set index 4 to null
 
-    sp::primitive_array<int> ar(std::move(data_buffer), std::move(validity));
+        sp::primitive_array<int> ar(std::move(data_buffer), std::move(validity));
 
-    std::vector<uint8_t> serialized_data = serialize_primitive_array(ar);
+        std::vector<uint8_t> serialized_data = serialize_primitive_array(ar);
 
-    CHECK(serialized_data.size() > 0);
+        CHECK(serialized_data.size() > 0);
 
-    sp::primitive_array<int> deserialized_ar = deserialize_primitive_array<int>(serialized_data);
+        sp::primitive_array<int> deserialized_ar = deserialize_primitive_array<int>(serialized_data);
 
-    CHECK_EQ(ar, deserialized_ar);
+        CHECK_EQ(ar, deserialized_ar);
 
-    compare_bitmap<int>(ar, deserialized_ar);
-    compare_metadata<int>(ar, deserialized_ar);
-}
+        compare_bitmap<int>(ar, deserialized_ar);
+        compare_metadata<int>(ar, deserialized_ar);
+    }
 
-TEST_CASE("Serialize and Deserialize primitive_array - with name and metadata")
-{
-    namespace sp = sparrow;
+    TEST_CASE("Serialize and Deserialize primitive_array - with name and metadata")
+    {
+        // Data buffer
+        sp::u8_buffer<int> data_buffer = {1, 2, 3};
 
-    // Data buffer
-    sp::u8_buffer<int> data_buffer = {1, 2, 3};
+        // Validity bitmap: All valid
+        sp::validity_bitmap validity(3, true);
 
-    // Validity bitmap: All valid
-    sp::validity_bitmap validity(3, true);
+        // Custom metadata
+        std::vector<sp::metadata_pair> metadata = {
+            {"key1", "value1"},
+            {"key2", "value2"}
+        };
 
-    // Custom metadata
-    std::vector<sp::metadata_pair> metadata = {
-        {"key1", "value1"},
-        {"key2", "value2"}
-    };
+        sp::primitive_array<int> ar(
+            std::move(data_buffer),
+            std::move(validity),
+            "my_named_array", // name
+            std::make_optional(std::vector<sparrow::metadata_pair>{{"key1", "value1"}, {"key2", "value2"}})
+        );
 
-    sp::primitive_array<int> ar(
-        std::move(data_buffer),
-        std::move(validity),
-        "my_named_array", // name
-        std::make_optional(std::vector<sparrow::metadata_pair>{{"key1", "value1"}, {"key2", "value2"}})
-    );
+        std::vector<uint8_t> serialized_data = serialize_primitive_array(ar);
 
-    std::vector<uint8_t> serialized_data = serialize_primitive_array(ar);
+        CHECK(serialized_data.size() > 0);
 
-    CHECK(serialized_data.size() > 0);
+        sp::primitive_array<int> deserialized_ar = deserialize_primitive_array<int>(serialized_data);
 
-    sp::primitive_array<int> deserialized_ar = deserialize_primitive_array<int>(serialized_data);
+        CHECK_EQ(ar, deserialized_ar);
 
-    CHECK_EQ(ar, deserialized_ar);
-
-    compare_bitmap<int>(ar, deserialized_ar);
-    compare_metadata<int>(ar, deserialized_ar);
+        compare_bitmap<int>(ar, deserialized_ar);
+        compare_metadata<int>(ar, deserialized_ar);
+    }
 }
