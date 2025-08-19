9e8b3fea7f80e842a1ac28cc8c2d99914c14ce5e
Hind Montassif <hind.montassif@gmail.com>
2025-08-19 13:48:10 +0200

    Use data_type_to_format in get_flatbuffer_type test

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -1,5 +1,7 @@
 #include "doctest/doctest.h"
 
+#include "sparrow.hpp"
+
 #include "utils.hpp"
 
 namespace sparrow_ipc
@@ -20,80 +22,80 @@ namespace sparrow_ipc
         flatbuffers::FlatBufferBuilder builder;
         SUBCASE("Null and Boolean types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "n").first, org::apache::arrow::flatbuf::Type::Null);
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "b").first, org::apache::arrow::flatbuf::Type::Bool);
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::NA)).first, org::apache::arrow::flatbuf::Type::Null);
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::BOOL)).first, org::apache::arrow::flatbuf::Type::Bool);
         }
 
         SUBCASE("Integer types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "c").first, org::apache::arrow::flatbuf::Type::Int); // INT8
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "C").first, org::apache::arrow::flatbuf::Type::Int); // UINT8
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "s").first, org::apache::arrow::flatbuf::Type::Int); // INT16
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "S").first, org::apache::arrow::flatbuf::Type::Int); // UINT16
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "i").first, org::apache::arrow::flatbuf::Type::Int); // INT32
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "I").first, org::apache::arrow::flatbuf::Type::Int); // UINT32
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "l").first, org::apache::arrow::flatbuf::Type::Int); // INT64
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "L").first, org::apache::arrow::flatbuf::Type::Int); // UINT64
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::INT8)).first, org::apache::arrow::flatbuf::Type::Int); // INT8
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::UINT8)).first, org::apache::arrow::flatbuf::Type::Int); // UINT8
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::INT16)).first, org::apache::arrow::flatbuf::Type::Int); // INT16
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::UINT16)).first, org::apache::arrow::flatbuf::Type::Int); // UINT16
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::INT32)).first, org::apache::arrow::flatbuf::Type::Int); // INT32
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::UINT32)).first, org::apache::arrow::flatbuf::Type::Int); // UINT32
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::INT64)).first, org::apache::arrow::flatbuf::Type::Int); // INT64
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::UINT64)).first, org::apache::arrow::flatbuf::Type::Int); // UINT64
         }
 
         SUBCASE("Floating Point types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "e").first, org::apache::arrow::flatbuf::Type::FloatingPoint); // HALF_FLOAT
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "f").first, org::apache::arrow::flatbuf::Type::FloatingPoint); // FLOAT
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "g").first, org::apache::arrow::flatbuf::Type::FloatingPoint); // DOUBLE
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::HALF_FLOAT)).first, org::apache::arrow::flatbuf::Type::FloatingPoint); // HALF_FLOAT
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::FLOAT)).first, org::apache::arrow::flatbuf::Type::FloatingPoint); // FLOAT
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::DOUBLE)).first, org::apache::arrow::flatbuf::Type::FloatingPoint); // DOUBLE
         }
 
         SUBCASE("String and Binary types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "u").first, org::apache::arrow::flatbuf::Type::Utf8); // STRING
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "U").first, org::apache::arrow::flatbuf::Type::LargeUtf8); // LARGE_STRING
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "z").first, org::apache::arrow::flatbuf::Type::Binary); // BINARY
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "Z").first, org::apache::arrow::flatbuf::Type::LargeBinary); // LARGE_BINARY
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::STRING)).first, org::apache::arrow::flatbuf::Type::Utf8); // STRING
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::LARGE_STRING)).first, org::apache::arrow::flatbuf::Type::LargeUtf8); // LARGE_STRING
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::BINARY)).first, org::apache::arrow::flatbuf::Type::Binary); // BINARY
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::LARGE_BINARY)).first, org::apache::arrow::flatbuf::Type::LargeBinary); // LARGE_BINARY
             CHECK_EQ(utils::get_flatbuffer_type(builder, "vu").first, org::apache::arrow::flatbuf::Type::Utf8View); // STRING_VIEW
             CHECK_EQ(utils::get_flatbuffer_type(builder, "vz").first, org::apache::arrow::flatbuf::Type::BinaryView); // BINARY_VIEW
         }
 
         SUBCASE("Date types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tdD").first, org::apache::arrow::flatbuf::Type::Date); // DATE_DAYS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tdm").first, org::apache::arrow::flatbuf::Type::Date); // DATE_MILLISECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::DATE_DAYS)).first, org::apache::arrow::flatbuf::Type::Date); // DATE_DAYS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::DATE_MILLISECONDS)).first, org::apache::arrow::flatbuf::Type::Date); // DATE_MILLISECONDS
         }
 
         SUBCASE("Timestamp types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tss:").first, org::apache::arrow::flatbuf::Type::Timestamp); // TIMESTAMP_SECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tsm:").first, org::apache::arrow::flatbuf::Type::Timestamp); // TIMESTAMP_MILLISECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tsu:").first, org::apache::arrow::flatbuf::Type::Timestamp); // TIMESTAMP_MICROSECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tsn:").first, org::apache::arrow::flatbuf::Type::Timestamp); // TIMESTAMP_NANOSECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::TIMESTAMP_SECONDS)).first, org::apache::arrow::flatbuf::Type::Timestamp); // TIMESTAMP_SECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::TIMESTAMP_MILLISECONDS)).first, org::apache::arrow::flatbuf::Type::Timestamp); // TIMESTAMP_MILLISECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::TIMESTAMP_MICROSECONDS)).first, org::apache::arrow::flatbuf::Type::Timestamp); // TIMESTAMP_MICROSECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::TIMESTAMP_NANOSECONDS)).first, org::apache::arrow::flatbuf::Type::Timestamp); // TIMESTAMP_NANOSECONDS
         }
 
         SUBCASE("Duration types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tDs").first, org::apache::arrow::flatbuf::Type::Duration); // DURATION_SECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tDm").first, org::apache::arrow::flatbuf::Type::Duration); // DURATION_MILLISECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tDu").first, org::apache::arrow::flatbuf::Type::Duration); // DURATION_MICROSECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tDn").first, org::apache::arrow::flatbuf::Type::Duration); // DURATION_NANOSECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::DURATION_SECONDS)).first, org::apache::arrow::flatbuf::Type::Duration); // DURATION_SECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::DURATION_MILLISECONDS)).first, org::apache::arrow::flatbuf::Type::Duration); // DURATION_MILLISECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::DURATION_MICROSECONDS)).first, org::apache::arrow::flatbuf::Type::Duration); // DURATION_MICROSECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::DURATION_NANOSECONDS)).first, org::apache::arrow::flatbuf::Type::Duration); // DURATION_NANOSECONDS
         }
 
         SUBCASE("Interval types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tiM").first, org::apache::arrow::flatbuf::Type::Interval); // INTERVAL_MONTHS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tiD").first, org::apache::arrow::flatbuf::Type::Interval); // INTERVAL_DAYS_TIME
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tin").first, org::apache::arrow::flatbuf::Type::Interval); // INTERVAL_MONTHS_DAYS_NANOSECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::INTERVAL_MONTHS)).first, org::apache::arrow::flatbuf::Type::Interval); // INTERVAL_MONTHS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::INTERVAL_DAYS_TIME)).first, org::apache::arrow::flatbuf::Type::Interval); // INTERVAL_DAYS_TIME
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::INTERVAL_MONTHS_DAYS_NANOSECONDS)).first, org::apache::arrow::flatbuf::Type::Interval); // INTERVAL_MONTHS_DAYS_NANOSECONDS
         }
 
         SUBCASE("Time types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "tts").first, org::apache::arrow::flatbuf::Type::Time); // TIME_SECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "ttm").first, org::apache::arrow::flatbuf::Type::Time); // TIME_MILLISECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "ttu").first, org::apache::arrow::flatbuf::Type::Time); // TIME_MICROSECONDS
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "ttn").first, org::apache::arrow::flatbuf::Type::Time); // TIME_NANOSECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::TIME_SECONDS)).first, org::apache::arrow::flatbuf::Type::Time); // TIME_SECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::TIME_MILLISECONDS)).first, org::apache::arrow::flatbuf::Type::Time); // TIME_MILLISECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::TIME_MICROSECONDS)).first, org::apache::arrow::flatbuf::Type::Time); // TIME_MICROSECONDS
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::TIME_NANOSECONDS)).first, org::apache::arrow::flatbuf::Type::Time); // TIME_NANOSECONDS
         }
 
         SUBCASE("List types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "+l").first, org::apache::arrow::flatbuf::Type::List); // LIST
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "+L").first, org::apache::arrow::flatbuf::Type::LargeList); // LARGE_LIST
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::LIST)).first, org::apache::arrow::flatbuf::Type::List); // LIST
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::LARGE_LIST)).first, org::apache::arrow::flatbuf::Type::LargeList); // LARGE_LIST
             CHECK_EQ(utils::get_flatbuffer_type(builder, "+vl").first, org::apache::arrow::flatbuf::Type::ListView); // LIST_VIEW
             CHECK_EQ(utils::get_flatbuffer_type(builder, "+vL").first, org::apache::arrow::flatbuf::Type::LargeListView); // LARGE_LIST_VIEW
             CHECK_EQ(utils::get_flatbuffer_type(builder, "+w:16").first, org::apache::arrow::flatbuf::Type::FixedSizeList); // FIXED_SIZED_LIST
@@ -102,8 +104,8 @@ namespace sparrow_ipc
 
         SUBCASE("Struct and Map types")
         {
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "+s").first, org::apache::arrow::flatbuf::Type::Struct_); // STRUCT
-            CHECK_EQ(utils::get_flatbuffer_type(builder, "+m").first, org::apache::arrow::flatbuf::Type::Map); // MAP
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::STRUCT)).first, org::apache::arrow::flatbuf::Type::Struct_); // STRUCT
+            CHECK_EQ(utils::get_flatbuffer_type(builder, sparrow::data_type_to_format(sparrow::data_type::MAP)).first, org::apache::arrow::flatbuf::Type::Map); // MAP
         }
 
         SUBCASE("Union types")
