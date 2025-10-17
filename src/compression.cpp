0d949aaaad843a114fc16ba733d2b596729213a6
Hind Montassif <hind.montassif@gmail.com>
2025-10-17 10:17:32 +0200

    Minor changes

--- src/compression.cpp
+++ src/compression.cpp
@@ -20,6 +20,49 @@ namespace sparrow_ipc
 //         }
 //     }
 
+    std::vector<std::uint8_t> lz4_compress(std::span<const std::uint8_t> data)
+    {
+        const std::int64_t uncompressed_size = data.size();
+        const size_t max_compressed_size = LZ4F_compressFrameBound(uncompressed_size, nullptr);
+        std::vector<std::uint8_t> compressed_data(max_compressed_size);
+        const size_t compressed_size = LZ4F_compressFrame(compressed_data.data(), max_compressed_size, data.data(), uncompressed_size, nullptr);
+        if (LZ4F_isError(compressed_size))
+        {
+            throw std::runtime_error("Failed to compress data with LZ4 frame format");
+        }
+        compressed_data.resize(compressed_size);
+        return compressed_data;
+    }
+
+    std::vector<std::uint8_t> lz4_decompress(std::span<const std::uint8_t> data)
+    {
+        if (data.size() < 8)
+        {
+            throw std::runtime_error("Invalid compressed data: missing decompressed size");
+        }
+        const std::int64_t decompressed_size = *reinterpret_cast<const std::int64_t*>(data.data());
+        const auto compressed_data = data.subspan(8);
+
+        if (decompressed_size == -1)
+        {
+            // TODO think of avoiding copy here
+            return {compressed_data.begin(), compressed_data.end()};
+        }
+
+        std::vector<std::uint8_t> decompressed_data(decompressed_size);
+        LZ4F_dctx* dctx = nullptr;
+        LZ4F_createDecompressionContext(&dctx, LZ4F_VERSION);
+        size_t compressed_size_in_out = compressed_data.size();
+        size_t decompressed_size_in_out = decompressed_size;
+        size_t result = LZ4F_decompress(dctx, decompressed_data.data(), &decompressed_size_in_out, compressed_data.data(), &compressed_size_in_out, nullptr);
+        if (LZ4F_isError(result) || (decompressed_size_in_out != (size_t)decompressed_size))
+        {
+            throw std::runtime_error("Failed to decompress data with LZ4 frame format");
+        }
+        LZ4F_freeDecompressionContext(dctx);
+        return decompressed_data;
+    }
+
     std::vector<std::uint8_t> compress(const org::apache::arrow::flatbuf::CompressionType compression_type, std::span<const std::uint8_t> data)
     {
         if (data.empty())
@@ -30,18 +73,14 @@ namespace sparrow_ipc
         {
             case org::apache::arrow::flatbuf::CompressionType::LZ4_FRAME:
             {
-                const std::int64_t uncompressed_size = data.size();
-                const size_t max_compressed_size = LZ4F_compressFrameBound(uncompressed_size, nullptr);
-                std::vector<std::uint8_t> compressed_data(max_compressed_size);
-                const size_t compressed_size = LZ4F_compressFrame(compressed_data.data(), max_compressed_size, data.data(), uncompressed_size, nullptr);
-                if (LZ4F_isError(compressed_size))
-                {
-                    throw std::runtime_error("Failed to compress data with LZ4 frame format");
-                }
-                compressed_data.resize(compressed_size);
-                return compressed_data;
+                return lz4_compress(data);
+            }
+            case org::apache::arrow::flatbuf::CompressionType::ZSTD:
+            {
+                throw std::runtime_error("Compression using zstd is not supported yet.");
             }
             default:
+                // TODO think of avoiding copy here
                 return {data.begin(), data.end()};
         }
     }
@@ -56,32 +95,14 @@ namespace sparrow_ipc
         {
             case org::apache::arrow::flatbuf::CompressionType::LZ4_FRAME:
             {
-                if (data.size() < 8)
-                {
-                    throw std::runtime_error("Invalid compressed data: missing decompressed size");
-                }
-                const std::int64_t decompressed_size = *reinterpret_cast<const std::int64_t*>(data.data());
-                const auto compressed_data = data.subspan(8);
-
-                if (decompressed_size == -1)
-                {
-                    return {compressed_data.begin(), compressed_data.end()};
-                }
-
-                std::vector<std::uint8_t> decompressed_data(decompressed_size);
-                LZ4F_dctx* dctx = nullptr;
-                LZ4F_createDecompressionContext(&dctx, LZ4F_VERSION);
-                size_t compressed_size_in_out = compressed_data.size();
-                size_t decompressed_size_in_out = decompressed_size;
-                size_t result = LZ4F_decompress(dctx, decompressed_data.data(), &decompressed_size_in_out, compressed_data.data(), &compressed_size_in_out, nullptr);
-                if (LZ4F_isError(result))
-                {
-                    throw std::runtime_error("Failed to decompress data with LZ4 frame format");
-                }
-                LZ4F_freeDecompressionContext(dctx);
-                return decompressed_data;
+                return lz4_decompress(data);
+            }
+            case org::apache::arrow::flatbuf::CompressionType::ZSTD:
+            {
+                throw std::runtime_error("Decompression using zstd is not supported yet.");
             }
             default:
+                // TODO think of avoiding copy here
                 return {data.begin(), data.end()};
         }
     }
--- tests/test_compression.cpp
+++ tests/test_compression.cpp
@@ -7,29 +7,59 @@
 
 namespace sparrow_ipc
 {
-    TEST_CASE("Compression and Decompression Round-trip")
+    TEST_SUITE("De/Compression")
     {
-        std::string original_string = "hello world, this is a test of compression and decompression!!";
-        std::vector<uint8_t> original_data(original_string.begin(), original_string.end());
-        const int64_t original_size = original_data.size();
+        TEST_CASE("Unsupported ZSTD de/compression")
+        {
+            std::string original_string = "some data to compress";
+            std::vector<uint8_t> original_data(original_string.begin(), original_string.end());
+            const auto compression_type = org::apache::arrow::flatbuf::CompressionType::ZSTD;
 
-        // Compress data
-        auto compression_type = org::apache::arrow::flatbuf::CompressionType::LZ4_FRAME;
-        std::vector<uint8_t> compressed_frame = compress(compression_type, original_data);
+            // Test compression with ZSTD
+            CHECK_THROWS_WITH_AS(sparrow_ipc::compress(compression_type, original_data), "Compression using zstd is not supported yet.", std::runtime_error);
 
-        CHECK_GT(compressed_frame.size(), 0);
-        CHECK_NE(compressed_frame, original_data);
+            // Test decompression with ZSTD
+            CHECK_THROWS_WITH_AS(sparrow_ipc::decompress(compression_type, original_data), "Decompression using zstd is not supported yet.", std::runtime_error);
+        }
 
-        // Manually create the IPC-formatted compressed buffer by adding the 8-byte prefix
-        std::vector<uint8_t> ipc_formatted_buffer;
-        ipc_formatted_buffer.reserve(sizeof(int64_t) + compressed_frame.size());
-        ipc_formatted_buffer.insert(ipc_formatted_buffer.end(), reinterpret_cast<const uint8_t*>(&original_size), reinterpret_cast<const uint8_t*>(&original_size) + sizeof(int64_t));
-        ipc_formatted_buffer.insert(ipc_formatted_buffer.end(), compressed_frame.begin(), compressed_frame.end());
+        TEST_CASE("Empty data")
+        {
+            const std::vector<uint8_t> empty_data;
+            const auto compression_type = org::apache::arrow::flatbuf::CompressionType::LZ4_FRAME;
 
-        // Decompress
-        std::vector<uint8_t> decompressed_data = decompress(compression_type, ipc_formatted_buffer);
+            // Test compression of empty data
+            auto compressed = compress(compression_type, empty_data);
+            CHECK(compressed.empty());
 
-        CHECK_EQ(decompressed_data.size(), original_data.size());
-        CHECK_EQ(decompressed_data, original_data);
+            // Test decompression of empty data
+            auto decompressed = decompress(compression_type, empty_data);
+            CHECK(decompressed.empty());
+        }
+
+        TEST_CASE("Data compression and decompression round-trip")
+        {
+            std::string original_string = "hello world, this is a test of compression and decompression!!";
+            std::vector<uint8_t> original_data(original_string.begin(), original_string.end());
+            const int64_t original_size = original_data.size();
+
+            // Compress data
+            auto compression_type = org::apache::arrow::flatbuf::CompressionType::LZ4_FRAME;
+            std::vector<uint8_t> compressed_frame = compress(compression_type, original_data);
+
+            CHECK_GT(compressed_frame.size(), 0);
+            CHECK_NE(compressed_frame, original_data);
+
+            // Manually create the IPC-formatted compressed buffer by adding the 8-byte prefix
+            std::vector<uint8_t> ipc_formatted_buffer;
+            ipc_formatted_buffer.reserve(sizeof(int64_t) + compressed_frame.size());
+            ipc_formatted_buffer.insert(ipc_formatted_buffer.end(), reinterpret_cast<const uint8_t*>(&original_size), reinterpret_cast<const uint8_t*>(&original_size) + sizeof(int64_t));
+            ipc_formatted_buffer.insert(ipc_formatted_buffer.end(), compressed_frame.begin(), compressed_frame.end());
+
+            // Decompress
+            std::vector<uint8_t> decompressed_data = decompress(compression_type, ipc_formatted_buffer);
+
+            CHECK_EQ(decompressed_data.size(), original_data.size());
+            CHECK_EQ(decompressed_data, original_data);
+        }
     }
 }
