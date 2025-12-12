6b65ecc8e2ce442c7d5fcd049501339b225a74de
Alexis Placet <alexis.placet@quantstack.net>
2025-12-12 13:46:37 +0100

    Avoid code duplication

--- include/sparrow_ipc/deserialize_primitive_array.hpp
+++ include/sparrow_ipc/deserialize_primitive_array.hpp
@@ -1,16 +1,13 @@
 #pragma once
 
 #include <optional>
-#include <unordered_set>
 #include <vector>
 
 #include <sparrow/arrow_interface/arrow_array_schema_proxy.hpp>
 #include <sparrow/primitive_array.hpp>
 
 #include "Message_generated.h"
-#include "sparrow_ipc/arrow_interface/arrow_array.hpp"
-#include "sparrow_ipc/arrow_interface/arrow_schema.hpp"
-#include "sparrow_ipc/deserialize_utils.hpp"
+#include "sparrow_ipc/deserialize_array_impl.hpp"
 
 namespace sparrow_ipc
 {
@@ -24,58 +21,13 @@ namespace sparrow_ipc
         size_t& buffer_index
     )
     {
-        const std::string_view format = data_type_to_format(
-            sparrow::detail::get_data_type_from_array<sparrow::primitive_array<T>>::get()
-        );
-        
-        // Set up flags based on nullable
-        std::optional<std::unordered_set<sparrow::ArrowFlag>> flags;
-        if (nullable)
-        {
-            flags = std::unordered_set<sparrow::ArrowFlag>{sparrow::ArrowFlag::NULLABLE};
-        }
-        
-        ArrowSchema schema = make_non_owning_arrow_schema(
-            format,
-            name.data(),
+        return detail::deserialize_non_owning_simple_array<sparrow::primitive_array, T>(
+            record_batch,
+            body,
+            name,
             metadata,
-            flags,
-            0,
-            nullptr,
-            nullptr
+            nullable,
+            buffer_index
         );
-
-        const auto compression = record_batch.compression();
-        std::vector<arrow_array_private_data::optionally_owned_buffer> buffers;
-
-        auto validity_buffer_span = utils::get_buffer(record_batch, body, buffer_index);
-        auto data_buffer_span = utils::get_buffer(record_batch, body, buffer_index);
-
-        if (compression)
-        {
-            buffers.push_back(utils::get_decompressed_buffer(validity_buffer_span, compression));
-            buffers.push_back(utils::get_decompressed_buffer(data_buffer_span, compression));
-        }
-        else
-        {
-            buffers.push_back(validity_buffer_span);
-            buffers.push_back(data_buffer_span);
-        }
-
-        // TODO bitmap_ptr is not used anymore... Leave it for now, and remove later if no need confirmed
-        const auto [bitmap_ptr, null_count] = utils::get_bitmap_pointer_and_null_count(validity_buffer_span, record_batch.length());
-
-        ArrowArray array = make_arrow_array<arrow_array_private_data>(
-            record_batch.length(),
-            null_count,
-            0,
-            0,
-            nullptr,
-            nullptr,
-            std::move(buffers)
-        );
-
-        sparrow::arrow_proxy ap{std::move(array), std::move(schema)};
-        return sparrow::primitive_array<T>{std::move(ap)};
     }
 }
