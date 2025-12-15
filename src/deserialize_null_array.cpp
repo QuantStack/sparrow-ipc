1f1d9e67bd4350badf2dbcfd686bb1baac1711fe
Alexis Placet <alexis.placet@quantstack.net>
2025-12-15 01:12:05 -0800

    Add support of null array (#68)

---
+++ src/deserialize_null_array.cpp
@@ -0,0 +1,45 @@
+#include "sparrow_ipc/deserialize_null_array.hpp"
+
+#include <unordered_set>
+#include <sparrow/null_array.hpp>
+
+#include "sparrow_ipc/arrow_interface/arrow_array.hpp"
+#include "sparrow_ipc/arrow_interface/arrow_schema.hpp"
+
+namespace sparrow_ipc
+{
+    sparrow::null_array deserialize_non_owning_null(
+        const org::apache::arrow::flatbuf::RecordBatch& record_batch,
+        std::span<const uint8_t> body,
+        std::string_view name,
+        const std::optional<std::vector<sparrow::metadata_pair>>& metadata,
+        bool nullable,
+        size_t& buffer_index
+    )
+    {
+        const std::string_view format = data_type_to_format(
+            sparrow::detail::get_data_type_from_array<sparrow::null_array>::get()
+        );
+
+        // Set up flags based on nullable
+        std::optional<std::unordered_set<sparrow::ArrowFlag>> flags;
+        if (nullable)
+        {
+            flags = std::unordered_set<sparrow::ArrowFlag>{sparrow::ArrowFlag::NULLABLE};
+        }
+
+        ArrowSchema schema = make_non_owning_arrow_schema(format, name.data(), metadata, flags, 0, nullptr, nullptr);
+        std::vector<sparrow_ipc::arrow_array_private_data::optionally_owned_buffer> buffers;
+        ArrowArray array = make_arrow_array<sparrow_ipc::arrow_array_private_data>(
+            record_batch.length(),
+            record_batch.length(),
+            0,
+            0,
+            nullptr,
+            nullptr,
+            std::move(buffers)
+        );
+        sparrow::arrow_proxy ap{std::move(array), std::move(schema)};
+        return sparrow::null_array{std::move(ap)};
+    }
+}
